756fbdd48b360b9fd8070df2fc9ab304f56c2408
Hairul Lana <hairullana99@gmail.com>
2021-11-26 14:26:48 +0800

    change layout nota dinas maker

--- nota-dinas.php
+++ nota-dinas.php
@@ -41,115 +41,123 @@ if($_SESSION['role'] != 'admin' && $_SESSION['role'] != 'lab'){
 
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col">
 
             <form action="print.php" method="post">
 
                 <p class="header-tambah">Cetak Nota Dinas</p>
-                <div class="mb-3">
-                    <label for="nomor" class="form-label">Nomor</label>
-                    <input required name="nomor" type="text" class="form-control" id="nomor" placeholder="Nomor">
-                </div>
-                <div class="mb-3">
-                    <label for="yth" class="form-label">Yth</label>
-                    <input required name="yth" type="text" class="form-control" id="yth" placeholder="Yth">
-                </div>
-                <div class="mb-3">
-                    <label for="dari" class="form-label">Dari</label>
-                    <input required name="dari" type="text" class="form-control" id="dari" placeholder="Dari">
-                </div>
-                <div class="mb-3">
-                    <label for="hal" class="form-label">Hal</label>
-                    <input required name="hal" type="text" class="form-control" id="hal" placeholder="Hal">
-                </div>
-                <div class="mb-3">
-                    <label for="tanggal" class="form-label">Tanggal</label>
-                    <input required name="tanggal" type="text" class="form-control" id="tanggal" placeholder="Tanggal">
-                </div>
-                <div class="mb-3">
-                    <label for="tembusan">Tembusan</label>
-                    <textarea name="tembusan" required class="form-control" placeholder="Tembusan" id="tembusan" style="height: 100px"></textarea>
-                </div>
-            
-                <p class="header-secondary">1</p>
-                <div class="mb-3">
-                    <label for="tugas">Tugas</label>
-                    <textarea name="tugas" required class="form-control" placeholder="Tugas" id="tugas" style="height: 100px"></textarea>
-                </div>
-                <div class="mb-3">
-                    <label for="lokasi" class="form-label">lokasi</label>
-                    <input required name="lokasi" type="text" class="form-control" id="lokasi" placeholder="lokasi">
-                </div>
-                <div class="mb-3">
-                    <label for="selama" class="form-label">Selama</label>
-                    <input required name="selama" type="text" class="form-control" id="selama" placeholder="Selama">
-                </div>
-                <div class="mb-3">
-                    <label for="tanggal-berangkat" class="form-label">Tanggal Berangkat</label>
-                    <input required name="tanggal-berangkat" type="text" class="form-control" id="tanggal-berangkat" placeholder="Tanggal Berangkat">
-                </div>
+                <div class="row justify-content-center">
+                    <div class="col-md-4 mb-3">
+                        <label for="nomor" class="form-label">Nomor</label>
+                        <input required name="nomor" type="text" class="form-control" id="nomor" placeholder="Nomor">
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="hal" class="form-label">Hal</label>
+                        <input required name="hal" type="text" class="form-control" id="hal" placeholder="Hal">
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="tanggal" class="form-label">Tanggal</label>
+                        <input required name="tanggal" type="text" class="form-control" id="tanggal" placeholder="Tanggal">
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="yth" class="form-label">Yth</label>
+                        <input required name="yth" type="text" class="form-control" id="yth" placeholder="Yth">
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="dari" class="form-label">Dari</label>
+                        <input required name="dari" type="text" class="form-control" id="dari" placeholder="Dari">
+                    </div>
+                    <div class="col-md-12 mb-3">
+                        <label for="tembusan">Tembusan</label>
+                        <textarea name="tembusan" required class="form-control" placeholder="Tembusan" id="tembusan" style="height: 100px"></textarea>
+                    </div>
                 
-                <div class="mb-3">
-                    <label for="tanggal-kembali" class="form-label">Tanggal Kembali</label>
-                    <input required name="tanggal-kembali" type="text" class="form-control" id="tanggal-kembali" placeholder="Tanggal Kembali">
-                </div>
-                <div class="mb-3">
-                    <label for="sumber-dana" class="form-label">Sumber Dana</label>
-                    <input required name="sumber-dana" type="text" class="form-control" id="sumber-dana" placeholder="Sumber Dana">
-                </div>
-                <div class="mb-3">
-                    <label for="nama" class="form-label">Nama</label>
-                    <input required name="nama" type="text" class="form-control" id="nama" placeholder="Nama">
-                </div>
-                <div class="mb-3">
-                    <label for="nip" class="form-label">NIP</label>
-                    <input required name="nip" type="text" class="form-control" id="nip" placeholder="NIP">
-                </div>
-                <div class="mb-3">
-                    <label for="pangkat" class="form-label">Pangkat/Gol </label>
-                    <input required name="pangkat" type="text" class="form-control" id="pangkat" placeholder="Pangkat/Gol">
-                </div>
-                <div class="mb-3">
-                    <label for="jabatan" class="form-label">Jabatan </label>
-                    <input required name="jabatan" type="text" class="form-control" id="jabatan" placeholder="Jabatan">
-                </div>
-            
-                <div class="mb-3">
-                    <label for="nama2" class="form-label">Nama</label>
-                    <input required name="nama2" type="text" class="form-control" id="nama2" placeholder="Nama">
-                </div>
-                <div class="mb-3">
-                    <label for="nip2" class="form-label">NIP</label>
-                    <input required name="nip2" type="text" class="form-control" id="nip2" placeholder="NIP">
-                </div>
-                <div class="mb-3">
-                    <label for="pangkat2" class="form-label">Pangkat/Gol </label>
-                    <input required name="pangkat2" type="text" class="form-control" id="pangkat2" placeholder="Pangkat/Gol">
-                </div>
-                <div class="mb-3">
-                    <label for="jabatan2" class="form-label">Jabatan </label>
-                    <input required name="jabatan2" type="text" class="form-control" id="jabatan2" placeholder="Jabatan">
-                </div>
-            
-                <div class="mb-3">
-                    <label for="nama3" class="form-label">Nama</label>
-                    <input required name="nama3" type="text" class="form-control" id="nama3" placeholder="Nama">
-                </div>
-                <div class="mb-3">
-                    <label for="nip3" class="form-label">NIP</label>
-                    <input required name="nip3" type="text" class="form-control" id="nip3" placeholder="NIP">
-                </div>
-                <div class="mb-3">
-                    <label for="pangkat3" class="form-label">Pangkat/Gol </label>
-                    <input required name="pangkat3" type="text" class="form-control" id="pangkat3" placeholder="Pangkat/Gol">
-                </div>
-                <div class="mb-3">
-                    <label for="jabatan3" class="form-label">Jabatan </label>
-                    <input required name="jabatan3" type="text" class="form-control" id="jabatan3" placeholder="Jabatan">
-                </div>
+                    <p class="header-secondary">1</p>
+                    <div class="col-md-12 mb-3">
+                        <label for="tugas">Tugas</label>
+                        <textarea name="tugas" required class="form-control" placeholder="Tugas" id="tugas" style="height: 100px"></textarea>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="lokasi" class="form-label">Lokasi</label>
+                        <input required name="lokasi" type="text" class="form-control" id="lokasi" placeholder="lokasi">
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="selama" class="form-label">Selama</label>
+                        <input required name="selama" type="text" class="form-control" id="selama" placeholder="Selama">
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="sumber-dana" class="form-label">Sumber Dana</label>
+                        <input required name="sumber-dana" type="text" class="form-control" id="sumber-dana" placeholder="Sumber Dana">
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="tanggal-berangkat" class="form-label">Tanggal Berangkat</label>
+                        <input required name="tanggal-berangkat" type="text" class="form-control" id="tanggal-berangkat" placeholder="Tanggal Berangkat">
+                    </div>
+                    
+                    <div class="col-md-6 mb-3">
+                        <label for="tanggal-kembali" class="form-label">Tanggal Kembali</label>
+                        <input required name="tanggal-kembali" type="text" class="form-control" id="tanggal-kembali" placeholder="Tanggal Kembali">
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="nama" class="form-label">Nama</label>
+                        <input required name="nama" type="text" class="form-control" id="nama" placeholder="Nama">
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="nip" class="form-label">NIP</label>
+                        <input required name="nip" type="text" class="form-control" id="nip" placeholder="NIP">
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="pangkat" class="form-label">Pangkat/Gol </label>
+                        <input required name="pangkat" type="text" class="form-control" id="pangkat" placeholder="Pangkat/Gol">
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="jabatan" class="form-label">Jabatan </label>
+                        <input required name="jabatan" type="text" class="form-control" id="jabatan" placeholder="Jabatan">
+                    </div>
+                
+                    <div class="col-md-6 mb-3">
+                        <label for="nama2" class="form-label">Nama 2</label>
+                        <input required name="nama2" type="text" class="form-control" id="nama2" placeholder="Nama">
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="nip2" class="form-label">NIP 2</label>
+                        <input required name="nip2" type="text" class="form-control" id="nip2" placeholder="NIP">
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="pangkat2" class="form-label">Pangkat/Gol 2</label>
+                        <input required name="pangkat2" type="text" class="form-control" id="pangkat2" placeholder="Pangkat/Gol">
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="jabatan2" class="form-label">Jabatan 2</label>
+                        <input required name="jabatan2" type="text" class="form-control" id="jabatan2" placeholder="Jabatan">
+                    </div>
+                
+                    <div class="col-md-6 mb-3">
+                        <label for="nama3" class="form-label">Nama 3</label>
+                        <input required name="nama3" type="text" class="form-control" id="nama3" placeholder="Nama">
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="nip3" class="form-label">NIP 3</label>
+                        <input required name="nip3" type="text" class="form-control" id="nip3" placeholder="NIP">
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="pangkat3" class="form-label">Pangkat/Gol 3</label>
+                        <input required name="pangkat3" type="text" class="form-control" id="pangkat3" placeholder="Pangkat/Gol">
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="jabatan3" class="form-label">Jabatan 3</label>
+                        <input required name="jabatan3" type="text" class="form-control" id="jabatan3" placeholder="Jabatan">
+                    </div>
 
-                <a href="index.php" class="btn btn-danger tambah" type="submit">Kembali</a>
-                <button class="btn btn-warning tambah" type="submit" name="cetak-nota">Cetak Laporan</button>
+                    <div class="col">
+                        <div class="row justify-content-center">
+                            <div class="col">
+                                <a href="index.php" class="btn btn-danger tambah" type="submit">Kembali</a>
+                                <button class="btn btn-warning tambah" type="submit" name="cetak-nota">Cetak Laporan</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </form>
         </div>
     </div>
